cb25d4c57f8445f617ef97951f31dfe89b9abb0f
AloySobek <supernova.wandering.star@gmail.com>
2024-10-18 19:37:50 +0300

    sync

--- src/tree.hpp
+++ src/tree.hpp
@@ -1,4 +1,30 @@
 #include <functional>
+#include <string>
+
+namespace tree {
+
+// Highly specialized tree
+class Binary {
+  public:
+    struct Node {
+        char data;
+
+        struct Node *left;
+        struct Node *right;
+    };
+
+    Binary() : root{nullptr} {}
+
+    void construct(std::string constructor) {}
+
+    bool eval() { return true; }
+
+    ~Binary() {}
+
+  private:
+    struct Node *root;
+};
+} // namespace tree
 
 template <typename T> class BinaryTree {
   public:
